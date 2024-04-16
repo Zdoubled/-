ff6405a2e2eba3c493b743c8cb83fdb0ffd017fc
z_double <1519595132@qq.com>
2024-04-16 23:34:23 +0800

    kafka同步es

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleFreemarkerServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleFreemarkerServiceImpl.java
@@ -4,13 +4,17 @@ import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.heima.article.service.ApArticleService;
 import com.heima.article.service.ArticleFreemarkerService;
+import com.heima.common.constants.ArticleConstants;
 import com.heima.file.service.FileStorageService;
 import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.search.vos.SearchArticleVo;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -58,6 +62,20 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
             //修改ap_articled表，保存static_url字段
             apArticleService.update(Wrappers.<ApArticle>lambdaUpdate().eq(ApArticle::getId, apArticle.getId()).set(ApArticle::getStaticUrl, path));
 
+            //发送消息，创建索引
+            createArticleESIndex(apArticle, content, path);
         }
     }
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private void createArticleESIndex(ApArticle apArticle, String content, String path) {
+        //属性拷贝
+        SearchArticleVo searchArticleVo = new SearchArticleVo();
+        BeanUtils.copyProperties(apArticle, searchArticleVo);
+        searchArticleVo.setContent(content);
+        searchArticleVo.setStaticUrl(path);
+        //发送消息
+        kafkaTemplate.send(ArticleConstants.ARTICLE_ES_SYNC_TOPIC, JSON.toJSONString(searchArticleVo));
+    }
+
 }
